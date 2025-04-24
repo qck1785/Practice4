24968b09061d1ff33ffc4bc0eb7b448417a50009
MIX PC <sashapanin2007@yandex.ru>
2025-04-25 00:06:12 +0400

    фыв

---
+++ ConsoleApp1/ConsoleApp23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число n: ");
+            int n = int.Parse(Console.ReadLine());
+            int square = 0;
+            for (int i = 1, odd = 1; i <= n; i++, odd += 2)
+            {
+                square += odd;
+            }
+            Console.WriteLine($"{n}² = {square}");
+            Console.WriteLine("Нажмите на любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp24/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSum = 0;
+            int odd = 1;
+            for (int num = 1; num <= 10; num++)
+            {
+                totalSum += odd;
+                odd += 2 * num + 1; 
+            }
+            Console.WriteLine($"Сумма квадратов от 1 до 10: {totalSum}");
+            Console.WriteLine("Нажмите на любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
